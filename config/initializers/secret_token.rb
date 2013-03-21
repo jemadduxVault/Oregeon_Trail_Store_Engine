0524b1ffb8db7af5fad8070907d826bf001c19e2
John Maddux <jemaddux@gmail.com>
2013-03-21 14:26:10 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreEngineVaderPups::Application.config.secret_token = 'd3ebf18c96db4deaa10ed525093fae5ed1e673ce3626e5c6e38c9a4ce4e7ad18c30cea8c01333884d7ff54fe3ecce2e546fc198acd6d33d14defc39c9e3694e9'
